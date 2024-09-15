e4cb981bf3d21941f46e58744ea7c8a1d197d449
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-09-15 21:48:30 +0530

    Time: 22 ms (99.49%), Space: 16.5 MB (99.21%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -16,8 +16,6 @@ class Solution:
         lines = lines.split('\n')
         no_of_lines = len(lines)
 
-        print(f'Total Lines : {no_of_lines}')
-
         for i,line in enumerate(lines):
             line = line.strip()
             no_of_spaces_present = len(line.split())-1
@@ -31,17 +29,12 @@ class Solution:
                 break
             
             spaces_per_word = ' '*max(extra_spaces//no_of_spaces_present,1)
-            print('Spaces per word:',len(spaces_per_word))
             line = spaces_per_word.join(line.split())
-            print(f'LINE: {line}')
 
             left_spaces = maxWidth - len(line)
-            print(f'LINE: {line} | {left_spaces}')
 
             if left_spaces:
-                print(f'LINE 1: {line}')
                 line = line.replace(spaces_per_word,spaces_per_word+' ',left_spaces)
-                print(f'LINE 2: {line}')
             
             lines[i] = line
                 
