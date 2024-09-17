fba25c9e7bb0de67e324c809c10b7f72a8c0296b
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-09-17 17:39:19 +0530

    Time: 116 ms (22.24%), Space: 16.4 MB (96.99%) - LeetHub

--- 3543-count-substrings-that-satisfy-k-constraint-i/3543-count-substrings-that-satisfy-k-constraint-i.py
+++ 3543-count-substrings-that-satisfy-k-constraint-i/3543-count-substrings-that-satisfy-k-constraint-i.py
@@ -1,6 +1,4 @@
 class Solution:
     def countKConstraintSubstrings(self, s: str, k: int) -> int:
-        def valid(string):
-            return string.count('1') <= k or string.count('0') <= k 
         
-        return sum(1 for i in range(len(s)) for j in range(i+1,len(s)+1) if valid(s[i:j]) )
+        return sum(1 for i in range(len(s)) for j in range(i+1,len(s)+1) if (s[i:j].count('1') <= k or s[i:j].count('0') <= k) )
