e4eb1896318a41a85b6ae53470fd913c8afe1d30
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-09-17 18:20:29 +0530

    Time: 55 ms (19.09%), Space: 16.5 MB (27.44%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        def count(n):
+            return nums.count(n)
+
+        nums = sorted(set(nums), key = count,reverse=True)
+
+        return [nums[0],nums[1]]
