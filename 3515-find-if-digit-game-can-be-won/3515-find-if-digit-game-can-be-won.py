4afb9d708ce36bd59f86ba25136faa7c8da74bab
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-09-17 18:34:17 +0530

    Time: 58 ms (45.65%), Space: 16.6 MB (23.44%) - LeetHub

--- 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.py
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.py
@@ -1,4 +1,6 @@
 class Solution:
     def canAliceWin(self, nums: List[int]) -> bool:
-        return not (sum(i for i in nums if i < 10 ) == sum(i for i in nums if i > 9 ))
-            
+        if sum(i for i in nums if i < 10 ) == sum(i for i in nums if i > 9 ):
+            return False
+        return True
+        
