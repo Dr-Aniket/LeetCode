5edcef138095c012ce1f5cf62e0a132916d97c53
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-09-17 17:38:24 +0530

    Time: 142 ms (14.78%), Space: 16.5 MB (43.24%) - LeetHub

--- 3543-count-substrings-that-satisfy-k-constraint-i/3543-count-substrings-that-satisfy-k-constraint-i.py
+++ 3543-count-substrings-that-satisfy-k-constraint-i/3543-count-substrings-that-satisfy-k-constraint-i.py
@@ -3,11 +3,4 @@ class Solution:
         def valid(string):
             return string.count('1') <= k or string.count('0') <= k 
         
-        count = 0
-        
-        for i in range(len(s)):
-            for j in range(i+1,len(s)+1):
-                if valid(s[i:j]):
-                    count += 1
-
-        return count 
+        return sum(1 for i in range(len(s)) for j in range(i+1,len(s)+1) if valid(s[i:j]) )
