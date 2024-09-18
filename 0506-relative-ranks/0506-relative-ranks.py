a681a2a7ac5ed002499019d76c7721ffd8cc5c61
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-09-18 16:24:01 +0530

    Time: 267 ms (15.58%), Space: 17.7 MB (48.84%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        positions = sorted(score,reverse = 1)
+        def place(i):
+            i = positions.index(i) + 1
+            if i == 1:
+                return 'Gold Medal'
+            elif i == 2:
+                return 'Silver Medal'
+            elif i == 3:
+                return 'Bronze Medal'
+            else:
+                return str(i)
+
+        return [place(i) for i in score]
