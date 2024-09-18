f5f21ca6dbb512c4bea4a2a3ccd7a102ed06f633
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-09-18 21:03:43 +0530

    Time: 140 ms (14.83%), Space: 35 MB (5.91%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,5 +1,3 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        for i in nums:
-            if nums.count(i) == 1:
-                return i
+        return eval('^'.join(map(str,nums)))
