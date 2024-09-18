cd790b28e1667237147a0b0dd3a9fa4103e00f36
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-09-18 16:53:12 +0530

    Time: 5661 ms (8.45%), Space: 18 MB (64.72%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        ans = [-1,-1]
+        for i in range(1,len(nums)+1):
+            count = nums.count(i)
+            if count == 2:
+                ans[0] = i
+            elif count == 0:
+                ans[1] = i
+            if ans[0] != -1 and ans[1] != -1:
+                break
+        
+        return ans
