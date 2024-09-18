0cddf1e9bbc749c4b6acc044315d4eadbf843e25
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-09-18 20:58:53 +0530

    Time: 4333 ms (5.01%), Space: 19.1 MB (53.24%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,3 +1,5 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return sorted(nums,key=lambda a: nums.count(a))[0]
+        for i in nums:
+            if nums.count(i) == 1:
+                return i
