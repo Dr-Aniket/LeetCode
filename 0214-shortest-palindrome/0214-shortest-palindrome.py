ca7eac677b24a5811ef9e34ec580f03095104d01
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-09-20 21:05:17 +0530

    Time: 680 ms (19.65%), Space: 17.1 MB (35.48%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,25 +1,16 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        # if not s:
-        #     return s
-        # append_at = 0
-        # invert_s = s[::-1]
-        # for c in invert_s:
-        #     if s == s[::-1]:
-        #         return s
-        #     else:
-        #         s = s[:append_at] + c + s[append_at:]
-        #         append_at += 1
-
         if not s:
             return s
+        append_at = 0
         invert_s = s[::-1]
-        s = '#'+s
         for c in invert_s:
-            if s.replace('#','') == s.replace('#','')[::-1]:
-                return s.replace('#','')
+            if s == s[::-1]:
+                return s
             else:
-                s = s.replace('#',c+'#')
+                s = s[:append_at] + c + s[append_at:]
+                append_at += 1
+
 
         
         
