40584caada5b995c38293097dfbf4830c8e64057
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-09-20 20:58:16 +0530

    Time: 1554 ms (7.47%), Space: 18.4 MB (29.48%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -2,12 +2,13 @@ class Solution:
     def shortestPalindrome(self, s: str) -> str:
         if not s:
             return s
+        s = list(s)
         append_at = 0
         for c in s[::-1]:
             if s == s[::-1]:
-                return s
+                return ''.join(s)
             else:
-                s = s[:append_at] + c + s[append_at:]
+                s.insert(append_at,c)
                 append_at += 1
         
 
