fd08dd5e5527b26a218e105f8ae27a1894ad6f46
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-10-02 01:08:08 +0530

    Time: 127 ms (51.32%), Space: 73.6 MB (42.63%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -19,12 +19,12 @@ public:
                     return false;
                 }
             }
-            // Special case for remainder k/2 when k is even, there must be an even number of such elements
-            else if (k % 2 == 0 && rem == k / 2) {
-                if (remainder_count[rem] % 2 != 0) {
-                    return false;
-                }
-            }
+            // // Special case for remainder k/2 when k is even, there must be an even number of such elements
+            // else if (k % 2 == 0 && rem == k / 2) {
+            //     if (remainder_count[rem] % 2 != 0) {
+            //         return false;
+            //     }
+            // }
             else {
                 // For other remainders, the number of elements with remainder 'rem' must match
                 // the number of elements with remainder 'k - rem'
