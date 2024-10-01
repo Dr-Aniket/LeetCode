118312f0a2f929aaa0a836214fae93ecad65be5a
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2024-10-02 01:07:52 +0530

    Time: 127 ms (51.32%), Space: 73.6 MB (42.63%) - LeetHub

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        unordered_map<int, int> remainder_count;
+
+        // Count the remainders
+        for (int num : arr) {
+            int remainder = ((num % k) + k) % k;  // Handle negative numbers correctly
+            remainder_count[remainder]++;
+        }
+
+        // Check pairs
+        for (const auto& entry : remainder_count) {
+            int rem = entry.first;
+
+            // Special case for remainder 0, there must be an even number of such elements
+            if (rem == 0) {
+                if (remainder_count[rem] % 2 != 0) {
+                    return false;
+                }
+            }
+            // Special case for remainder k/2 when k is even, there must be an even number of such elements
+            else if (k % 2 == 0 && rem == k / 2) {
+                if (remainder_count[rem] % 2 != 0) {
+                    return false;
+                }
+            }
+            else {
+                // For other remainders, the number of elements with remainder 'rem' must match
+                // the number of elements with remainder 'k - rem'
+                if (remainder_count[rem] != remainder_count[k - rem]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
