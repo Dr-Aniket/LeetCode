1c85005e8b53a13333c046a53f3694d025be16ec
Dr. Aniket <75909206+Dr-Aniket@users.noreply.github.com>
2025-03-06 11:32:26 +0530

    Time: 8 ms (57.93%), Space: 18.3 MB (23.37%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
@@ -1,21 +1,30 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         n = len(grid)
-
-        ans = []
-        pre = []
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] in pre:
-                    ans.append(grid[i][j])
-                else:
-                    pre.append(grid[i][j])
+        total_elements = n * n
         
-        pre.sort()
-
-        for i, ele in enumerate(pre+[0]):
-            if i+1 != ele:
-                ans.append(i+1)
+        # Expected sum and sum of squares
+        expected_sum = (total_elements * (total_elements + 1)) // 2
+        expected_sum_sq = (total_elements * (total_elements + 1) * (2 * total_elements + 1)) // 6
+        
+        actual_sum = 0
+        actual_sum_sq = 0
+        num_counts = {}
+        
+        # Compute actual sum and sum of squares
+        for row in grid:
+            for num in row:
+                actual_sum += num
+                actual_sum_sq += num * num
+                num_counts[num] = num_counts.get(num, 0) + 1
+        
+        # Find the repeating number
+        for key, value in num_counts.items():
+            if value == 2:
+                a = key  # Repeated number
                 break
         
-        return(ans)
+        # Compute missing number using derived formulas
+        b = a + (expected_sum - actual_sum)
+        
+        return [a, b]
